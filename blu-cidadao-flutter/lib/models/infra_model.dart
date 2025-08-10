030327130fec1407209e8b3c1407ddb6d081246b
Beatriz Conrat Müller da Silva <bia2003conrat@gmail.com>
2025-08-10 18:44:58 -0300

    commit pagina infraestrutura

---
+++ blu-cidadao-flutter/lib/models/infra_model.dart
@@ -0,0 +1,6 @@
+class InfraService {
+  final String nome;
+  final String url;
+
+  InfraService({required this.nome, required this.url});
+}
---
+++ blu-cidadao-flutter/lib/models/infra_service.dart
@@ -0,0 +1,20 @@
+import '../models/infra_model.dart';
+
+class InfraServiceData {
+  static List<InfraService> getServicos() {
+    return [
+      InfraService(
+        nome: 'Água (SAMAE)',
+        url: 'https://www.samae.com.br/area-do-cliente',
+      ),
+      InfraService(
+        nome: 'Esgoto (SAMAE)',
+        url: 'https://www.samae.com.br/area-do-cliente',
+      ),
+      InfraService(
+        nome: 'Energia (CELESC)',
+        url: 'https://servicos.celesc.com.br/portalcliente/web/login',
+      ),
+    ];
+  }
+}
