476bff9d0d4e8150495851e63e39a1178d5811b3
Gabriel Abreu <120991872+gabrielvsabreu@users.noreply.github.com>
2025-08-11 09:51:24 -0300

    commit - splash page update

--- blu-cidadao-flutter/lib/pages/splash/splash_page.dart
+++ blu-cidadao-flutter/lib/pages/splash/splash_page.dart
@@ -13,7 +13,7 @@ class _SplashPageState extends State<SplashPage> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(const Duration(seconds: 30), () {
+    Future.delayed(const Duration(seconds: 10), () {
       Navigator.pushReplacement(
         // ignore: use_build_context_synchronously
         context,
