cf03f144aeb5acbe29da4283f8e621c8266d9990
Gabriel Abreu <120991872+gabrielvsabreu@users.noreply.github.com>
2025-08-11 10:37:04 -0300

    commit - educação update

--- blu-cidadao-flutter/lib/pages/escolas/escolas_service.dart
+++ blu-cidadao-flutter/lib/pages/escolas/escolas_service.dart
@@ -4,7 +4,9 @@ import 'package:http/http.dart' as http;
 
 class EscolaService {
   Future<List<Escola>> fetchEscolas() async {
-    final uri = Uri.parse('http://10.0.2.2:8080/escolas');
+    final uri = Uri.parse(
+      'https://api-escolas-production.up.railway.app/escolas',
+    );
     final response = await http.get(uri);
 
     if (response.statusCode == 200) {
