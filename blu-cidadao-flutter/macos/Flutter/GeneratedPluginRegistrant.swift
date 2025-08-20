9bd4502e326383e15fef439abc055dfc2f7867e7
Gabriel Abreu <120991872+gabrielvsabreu@users.noreply.github.com>
2025-08-20 19:58:52 -0300

    Revert "commit restauração"
    This reverts commit 2fd49186d7962e39fcc06b2b1421d8c2369ddaf6.

--- blu-cidadao-flutter/macos/Flutter/GeneratedPluginRegistrant.swift
+++ blu-cidadao-flutter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,18 +5,12 @@
 import FlutterMacOS
 import Foundation
 
-import device_info_plus
-import file_saver
 import file_selector_macos
 import path_provider_foundation
-import syncfusion_pdfviewer_macos
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
-  FileSaverPlugin.register(with: registry.registrar(forPlugin: "FileSaverPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-  SyncfusionFlutterPdfViewerPlugin.register(with: registry.registrar(forPlugin: "SyncfusionFlutterPdfViewerPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
